9b02abe08718cee6f019d124a3d5f7aa7e8ff321
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-09-25 19:41:52 +0900

    ｋｏｓｓｉ（ｋａｋｕｊｉｔｕｎｉ）

---
+++ main.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+import os
+import re
+from urllib.parse import quote
+from bottle import Bottle, run, redirect
+
+app = Bottle()
+
+TEMPLATE_TXT = '{} うおおおおおおあああああああああああああああああああ！！！！！！！！！！！ (ﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘｭﾘｭﾘｭﾘｭﾘｭﾘｭ！！！！！！ﾌﾞﾂﾁﾁﾌﾞﾌﾞﾌﾞﾁﾁﾁﾁﾌﾞﾘﾘｲﾘﾌﾞﾌﾞﾌﾞﾌﾞｩｩｩｩｯｯｯ！！！！！！！)'
+TEMPLATE_URL = 'https://twitter.com/intent/tweet?text={}'
+
+def list_filter(config):
+    delimiter = r'[/\+]' or config
+    regexp = r'@?[0-9a-zA-Z_]+({}@?[0-9a-zA-Z_]+)*/*'.format(delimiter)
+
+    def to_py(match):
+        return [x.replace('@', '') for x in re.split(delimiter, match)
+                                   if len(x) > 0]
+    def to_url(strs):
+        return delimiter.join(strs)
+
+    return regexp, to_py, to_url
+
+app.router.add_filter('list', list_filter)
+
+@app.route('/<targets:list>')
+def butimili(targets):
+    replies = '@' + ' @'.join(targets)
+    return redirect(TEMPLATE_URL.format(quote(TEMPLATE_TXT.format(replies))))
+
+
+if __name__ == '__main__':
+    run(app,
+        host=os.environ.get('BUTIMILI_HOST', 'localhost'),
+        port=os.environ.get('BUTIMILI_PORT', 8080),)
